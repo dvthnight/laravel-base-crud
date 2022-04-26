cf8c1c5e12469c269678761995b418c0ddc5d305
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-26 17:54:11 +0200

    nuovo comic in seeder e completato file migration

--- database/migrations/2022_04_26_132359_create_comics_table.php
+++ database/migrations/2022_04_26_132359_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string("title",100);
+            $table->text("description")->nullable();
+            $table->text("thumb");
+            $table->string("price",30);
+            $table->string("series",100);
+            $table->string("sale_date",30);
+            $table->string("type",30);
             $table->timestamps();
         });
     }
--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,6 +12,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config("comics");
+
+        foreach($comics as $comic){ 
+            $comic = new Comic();
+            $comic->title = $comic["title"];
+            $comic->description = $comic["description"];
+            $comic->thumb = $comic["thumb"];
+            $comic->price = $comic["price"];
+            $comic->series = $comic["series"];
+            $comic->sale_date = $comic["sale_date"];
+            $comic->type = $comic["type"];
+            $comic->save();
+        }
     }
 }
