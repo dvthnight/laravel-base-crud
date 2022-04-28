09089e5a79f8a16040080cb2cb1b9e32580a4e14
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-28 16:24:59 +0200

    validazioone anceh su modifica

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,39 +9,49 @@
             <form action="{{route("comics.update",$comic)}}" method="POST">
                 @csrf
                 @method("PUT")
-                <div>
-                    <label for="name">Titolo</label>
-                    <input type="text" name="title" id="name" value="{{$comic->title}}" placeholder="Inserisci il titolo del fumetto">
+                <div class="form-group">
+                    <label for="name" class="form-label">Titolo</label>
+                    <input type="text" name="title" id="name" value="{{old("title") ? old("title") : $comic->title}}" class="@error('title') is-invalid @enderror form-control" placeholder="Inserisci il titolo del fumetto">
                 </div>
-                <div>
-                    <label for="description">Descrizione</label>
-                    <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci la descrizione del fumetto">
-                        {{$comic->title}}
+                <div class="form-group">
+                    <label for="description" class="form-label">Descrizione</label>
+                    <textarea name="description" id="description" cols="30" rows="10" class="@error('description') is-invalid @enderror form-control" placeholder="Inserisci la descrizione del fumetto">
+                        {{old("description") ? old("description") : $comic->description}}
                     </textarea>
                 </div>
-                <div>
-                    <label for="image">Immagine</label>
-                    <input type="text" name="thumb" id="image" value="{{$comic->thumb}}" placeholder="Inserisci url dell'immagine ">
+                <div class="form-group">
+                    <label for="image" class="form-label">Immagine</label>
+                    <input type="text" name="thumb" id="image" value="{{old("thumb") ? old("thumb") : $comic->thumb}}" class="@error('thumb') is-invalid @enderror form-control" placeholder="Inserisci url dell'immagine ">
                 </div>
                 <div>
-                    <label for="price">Prezzo</label>
-                    <input type="text" name="price" id="price" value="{{$comic->price}}" placeholder="Inserisci il prezzo del fumetto">
+                    <label for="price" class="form-label">Prezzo</label>
+                    <input type="text" name="price" id="price" value="{{old("price") ? old("price") : $comic->price}}" class="@error('price') is-invalid @enderror form-control" placeholder="Inserisci il prezzo del fumetto">
                 </div>
-                <div>
-                    <label for="series">Serie</label>
-                    <input type="text" name="series" id="series" value="{{$comic->series}}" placeholder="Inserisci la serie del fumetto">
+                <div class="form-group">
+                    <label for="series" class="form-label">Serie</label>
+                    <input type="text" name="series" id="series" value="{{old("series") ? old("series") : $comic->series}}" class="@error('series') is-invalid @enderror form-control" placeholder="Inserisci la serie del fumetto">
                 </div>
-                <div>
-                    <label for="date">Data</label>
-                    <input type="text" name="sale_date" id="date"value="{{$comic->sale_date}}"  placeholder="Inserisci la data del fumetto">
+                <div class="form-group">
+                    <label for="date" class="form-label">Data</label>
+                    <input type="text" name="sale_date" id="date"value="{{old("sale_date") ? old("sale_date") : $comic->sale_date}}" class="@error('sale_date') is-invalid @enderror form-control" placeholder="Inserisci la data del fumetto">
                 </div>
-                <div>
-                    <label for="type">Tipologia</label>
-                    <input type="text" name="type" id="type" value="{{$comic->type}}" placeholder="Inserisci la tipologia">
+                <div class="form-group">
+                    <label for="type" class="form-label">Tipologia</label>
+                    <input type="text" name="type" id="type" value="{{old("type") ? old("type") : $comic->type}}" class="@error('type') is-invalid @enderror form-control" placeholder="Inserisci la tipologia">
                 </div>
                 <button type="submit">
-                    Crea
+                    Salva
                 </button>
+
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             </form>
         </div>
     </main>
